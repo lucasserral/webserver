ad7cabfdaac358f2938f416c56eb231e80ae6cac
Lucas Serral <lucasser21@gmail.com>
2023-01-28 11:00:44 -0300

    [ADD] hbs partials & serve all sites

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import path from "path";
 import url from "url";
+import hbs from "hbs";
 
 const __filename = url.fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -8,21 +9,28 @@ const __dirname = path.dirname(__filename);
 const app = express();
 const port = 8080;
 
+const settings = {
+  name: "Lucas Serral",
+  title: "ExpressJS project with hbs",
+};
+
+hbs.registerPartials(__dirname + "/views/partials");
+
 app.set("view engine", "hbs");
 
 // serve static content.
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.render("home");
+  res.render("home", settings);
 });
 
 app.get("/generic", (req, res) => {
-  res.sendFile(__dirname + "/public/generic.html");
+  res.render("generic", settings);
 });
 
 app.get("/elements", (req, res) => {
-  res.sendFile(__dirname + "/public/elements.html");
+  res.render("elements", settings);
 });
 
 app.listen(port);
